f9a9c6a9119aef44d8d9b14d8b7157b60f0a32cd
Robel Shanbel <fklm.web1@gmail.com>
2023-03-08 07:17:37 +0300

    adding api getway and service registry

---
+++ naming-service/src/test/java/com/craftnaming/service/NamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craftnaming.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
